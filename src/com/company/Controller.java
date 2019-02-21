81204fe64abb34a61d82b9b71921dab34cb4ec40
Udjiin <udjinprofit@gmail.com>
2019-02-21 17:29:43 +0200

    Change range of the secret number

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 public class Controller {
 
-
-
     private Model model;
     private View view;
 
@@ -17,12 +15,19 @@ public class Controller {
   public void userProcess()
   {
       Scanner sc=new Scanner(System.in);
-      model.setPrimaryBarrier(view.MIN_NUMBER,view.MAX_NUMBER);
-      model.setSecretNumber();
-
 
       view.printMessage(view.GAME_NAME);
       view.printMessage(view.GAME_SENSE);
+while(!(view.MIN_NUMBER<view.MAX_NUMBER)) {
+    view.MIN_NUMBER = inputMinBarrier(sc);
+    view.MAX_NUMBER = inputMaxBarrier(sc);
+    if(view.MIN_NUMBER>view.MAX_NUMBER){
+    view.printMessage(view.MAX_BARRIER_LESS_MIN);
+    continue;
+    }
+}
+      model.setPrimaryBarrier(view.MIN_NUMBER,view.MAX_NUMBER);
+      model.setSecretNumber();
 
       while (model.checkValue(inputSupposedNumber(sc)));
 
@@ -30,7 +35,7 @@ public class Controller {
       view.printMessage(view.SECRET_NUMBER_WAS+model.getSecretNumber());
       view.printMessage(view.TRY_INFO+String.valueOf(model.getYourWay()));
   }
-public int inputSupposedNumber(Scanner sc) {
+    public int inputSupposedNumber(Scanner sc) {
     int res = 0;
     view.printMessage(view.INPUT_INT_NUMBER+String.valueOf(model.getMinNumber())+view.HYPHEN
             +String.valueOf(model.getMaxNumber())+view.DIAPASON);
@@ -49,6 +54,26 @@ public int inputSupposedNumber(Scanner sc) {
         break;
     }
     return res;
+    }
+    public int inputMinBarrier(Scanner sc){
+    int res=0;
+    view.printMessage(view.INPUT_MIN_BARRIER);
+        while (!sc.hasNextInt()) {
+            view.printMessage("\t"+view.WRONG_INPUT+"\n"+view.INPUT_MIN_BARRIER);
+            sc.next();
+        }res=sc.nextInt();
+    return res;
+  }
+
+    public int inputMaxBarrier(Scanner sc){
+        int res=0;
+        view.printMessage(view.INPUT_MAX_BARRIER);
+        while (!sc.hasNextInt()) {
+            view.printMessage("\t"+view.WRONG_INPUT+"\n"+view.INPUT_MAX_BARRIER);
+            sc.next();
+        }
+        res=sc.nextInt();
+        return res;
     }
 }
 
--- src/com/company/View.java
+++ src/com/company/View.java
@@ -2,20 +2,21 @@ package com.company;
 
 public class View {
   public   int MIN_NUMBER=0;
-  public   int MAX_NUMBER=100;
+  public   int MAX_NUMBER=0;
 
 public static String GAME_NAME="\t\t\tGuess Game";
-public static String GAME_SENSE="The task is to guess the number in 0--100 diapason";
+public static String GAME_SENSE="The task is to guess the number in chosen diapason";
 public static String INPUT_INT_NUMBER="Input suppose integer number in ";
 public static String HYPHEN=" -- ";
 public static String DIAPASON=" diapason";
 public static String WRONG_INPUT="Wrong input! Try again";
-public static String GREATER="Your number is greater then guess number";
-public static String LESS="Your number is less then guess number";
 public static String WIN="\t\tCongratulations!!!\n\t You guessed the secret number";
 public static String SECRET_NUMBER_WAS="\t\tThe secret number was: ";
 public static String TRY_INFO="\tInformation about the attempts:  ";
 public static String WRONG_DIAPOSON="Your number is not in   ";
+public static String INPUT_MAX_BARRIER="Input high limit of secret value(integer value):   ";
+public static String INPUT_MIN_BARRIER="Input low limit of secret value(integer value):   ";
+public static String MAX_BARRIER_LESS_MIN="\t\tWrong data!!!\nHigh limit is less than lower!";
 
 
 public void printMessage(String message)
